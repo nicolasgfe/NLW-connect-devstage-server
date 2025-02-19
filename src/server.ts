03edbaab013f0d3185c77a4ed13f98be62cbad90
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2025-02-19 00:50:00 -0300

    adicionando validação serialização rota de teste

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,40 @@
-import {fastify} from "fastify"
+import { fastify } from "fastify"
+import { fastifyCors } from "@fastify/cors"
+import { validatorCompiler, serializerCompiler, ZodTypeProvider } from "fastify-type-provider-zod"
+import { z } from "zod"
 
-const app = fastify()
 
-app.get("/hello", () => {
-	return "Hello World"
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+	origin: true // "http://localhost:3000"
+})
+
+
+app.post("/subscriptions", {
+	schema: {
+		body: z.object({
+			name: z.string(),
+			email: z.string().email(),
+		}),
+		response: {
+			201: z.object({
+				name: z.string(),
+				email: z.string(),
+			})
+		}
+	}
+}, async (request, reply) => {
+	const { name, email } = request.body
+
+	return reply.status(201).send({
+		name,
+		email
+	})
+
 })
 
 app.listen({
