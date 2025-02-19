a526dc178d6b7d409bf0d814798134ad593abd20
set <set>
2025-02-19 08:23:10 -0300

    ajustando indentação

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
-import { fastify } from "fastify"
-import { fastifyCors } from "@fastify/cors"
-import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from "fastify-type-provider-zod"
-import { fastifySwagger } from "@fastify/swagger"
-import { fastifySwaggerUi } from "@fastify/swagger-ui"
-import { subscribeToEventRoute } from "./routes/subscribe-to-event-route"
-import { env } from "./env"
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -12,27 +17,29 @@ app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors, {
-	origin: true // "http://localhost:3000"
+  origin: true, // "http://localhost:3000"
 })
 
 app.register(fastifySwagger, {
-	openapi: {
-		info: {
-			title: "NLW-connect",
-			version: "0.0.1"
-		}
-	},
-	transform: jsonSchemaTransform
+  openapi: {
+    info: {
+      title: 'NLW-connect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
 })
 
 app.register(fastifySwaggerUi, {
-	routePrefix: "/docs"
+  routePrefix: '/docs',
 })
 
 app.register(subscribeToEventRoute)
 
-app.listen({
-	port: env.PORT
-}).then(() => {
-	console.log("HTTP Server Running");
-})
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })
