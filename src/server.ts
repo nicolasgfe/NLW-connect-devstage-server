1de80f8f5c803fd66e03bf8c0be641c6b0e1cdd7
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2025-02-19 00:09:59 -0300

    configurando primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("hello World");
+import {fastify} from "fastify"
+
+const app = fastify()
+
+app.get("/hello", () => {
+	return "Hello World"
+})
+
+app.listen({
+	port: 3333
+}).then(() => {
+	console.log("HTTP Server Running");
+})
