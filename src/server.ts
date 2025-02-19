2827fb7da3417fe233efc30cd8a1285654a87861
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2025-02-19 00:57:48 -0300

    Adicionando estrutura de pasta para rota

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { fastify } from "fastify"
 import { fastifyCors } from "@fastify/cors"
 import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from "fastify-type-provider-zod"
-import { z } from "zod"
 import { fastifySwagger } from "@fastify/swagger"
 import { fastifySwaggerUi } from "@fastify/swagger-ui"
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -28,28 +28,7 @@ app.register(fastifySwaggerUi, {
 	routePrefix: "/docs"
 })
 
-app.post("/subscriptions", {
-	schema: {
-		body: z.object({
-			name: z.string(),
-			email: z.string().email(),
-		}),
-		response: {
-			201: z.object({
-				name: z.string(),
-				email: z.string(),
-			})
-		}
-	}
-}, async (request, reply) => {
-	const { name, email } = request.body
-
-	return reply.status(201).send({
-		name,
-		email
-	})
-
-})
+app.register(subscribeToEventRoute)
 
 app.listen({
 	port: 3333
